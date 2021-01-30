376695dd8f178fdf8ae3dc506556d3d9acb10a2e
Vincent <ursinov@sheridancollege.ca>
2021-01-29 20:11:37 -0500

    First commit - PartTime

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,6 +12,26 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private String numHours;
+
+    /**
+     * Get the value of numHours
+     *
+     * @return the value of numHours
+     */
+    public String getNumHours() {
+        return numHours;
+    }
+
+    /**
+     * Set the value of numHours
+     *
+     * @param numHours new value of numHours
+     */
+    public void setNumHours(String numHours) {
+        this.numHours = numHours;
+    }
+
 
     /**
      * Get the value of name
